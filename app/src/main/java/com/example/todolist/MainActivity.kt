3d4b35b266578364b554dfd97d21add9406d80b5
Deepshika Ghale <58839663+DeepshikaGhale@users.noreply.github.com>
2023-03-14 23:38:09 -0400

    Create check box for added items

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.Toast
+import com.example.todolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    var todoList: Array<String> = arrayOf<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater) //
+        setContentView(binding.root)
+
+        binding.addBtn.setOnClickListener(){
+            if(binding.itemsInput.text.isNotEmpty()){
+                todoList += binding.itemsInput.text.toString()
+                addCheckBox(binding.itemsInput.text.toString())
+                binding.itemsInput.text.append("")
+            }else{
+                Toast.makeText(this, "Please add any input.", Toast.LENGTH_SHORT).show()
+            }        }
+
+    }
+
+    fun addCheckBox(todoData: String){
+            val checkBox = CheckBox(this)
+            checkBox.text = todoData
+            checkBox.isChecked = false
+            binding.todoList.addView(checkBox)
     }
 }
